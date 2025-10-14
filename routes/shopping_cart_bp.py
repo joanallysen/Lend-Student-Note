809163db096a7f91e0ecb1db1dfde0c66199a6db
JasonnnW056 <williamjason056@gmail.com>
2025-10-14 15:43:07 +1300

    Shopping cart remove item, add  and subtract quantity functionalties added

--- routes/shopping_cart_bp.py
+++ routes/shopping_cart_bp.py
@@ -4,12 +4,25 @@ from models import db, Cart, CartItem, Note
 shopping_cart = Blueprint('shopping_cart',__name__)
 
 @shopping_cart.route('/mall')
-def cart():
+def mall():
     all_notes = Note.query.all()
 
     return render_template('mall.html',notes=all_notes)
 
-@shopping_cart.route('/add_to_cart/<string:note_id>', methods=["GET"])
+@shopping_cart.route('/user_cart')
+def user_cart():
+    user_id= session.get('user_id')
+    
+    cart_exist = Cart.query.filter(Cart.user_id == user_id).first()
+
+    if cart_exist:
+        content= cart_exist.items
+    else:
+        content="Nothing"
+
+    return render_template('user_cart.html', items = content)
+
+@shopping_cart.route('/add_to_cart/<int:note_id>', methods=["GET"])
 def add_to_cart(note_id):
     
     user_id = session.get('user_id')
@@ -32,17 +45,49 @@ def add_to_cart(note_id):
         db.session.add(new_item)
     
     db.session.commit()
-    return redirect(url_for('shopping_cart.cart'))
+    return redirect(url_for('shopping_cart.mall'))
 
-@shopping_cart.route('/user_cart')
-def user_cart():
+@shopping_cart.route('/add_quantity/<int:note_id>')
+def add_quantity(note_id):
     user_id= session.get('user_id')
+    user_cart = Cart.query.filter(Cart.user_id == user_id).first()
+    user_cart_id = user_cart.cart_id
+
+    #Search user item based on the note id
+    selected_item = CartItem.query.filter(CartItem.cart_id == user_cart_id, CartItem.note_id == note_id).first()
+
+    selected_item.quantity += 1
+
+    db.session.commit()
+    return redirect(url_for('shopping_cart.user_cart'))
+
+@shopping_cart.route('/subtract_quantity/<int:note_id>')
+def subtract_quantity(note_id):
+    user_id= session.get('user_id')
+    user_cart = Cart.query.filter(Cart.user_id == user_id).first()
+    user_cart_id = user_cart.cart_id
+
+    #Search user item based on the note id
+    selected_item = CartItem.query.filter(CartItem.cart_id == user_cart_id, CartItem.note_id == note_id).first()
+
+    selected_item.quantity -= 1
+
+    if selected_item.quantity == 0:
+        remove_item(note_id)
+
+    db.session.commit()
+    return redirect(url_for('shopping_cart.user_cart'))
+
+@shopping_cart.route('/remove_item/<int:note_id>')
+def remove_item(note_id):
+    user_id = session.get('user_id')
+    user_cart = Cart.query.filter(Cart.user_id == user_id).first()
+    user_cart_id = user_cart.cart_id
     
-    cart_exist = Cart.query.filter(Cart.user_id == user_id).first()
+    #Search user item based on the note id
+    selected_item = CartItem.query.filter(CartItem.cart_id == user_cart_id, CartItem.note_id == note_id).first()
+    db.session.delete(selected_item)
+    db.session.commit()
 
-    if cart_exist:
-        content= cart_exist.items
-    else:
-        content="Nothing"
+    return redirect(url_for('shopping_cart.user_cart'))
 
-    return render_template('user_cart.html', items = content)
