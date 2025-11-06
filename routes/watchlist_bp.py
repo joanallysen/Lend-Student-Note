2ef1e7e63b8ebc380f7e3cfc6d0c44bb6fe95341
JasonnnW056 <williamjason056@gmail.com>
2025-11-06 16:22:30 +1300

    Fixed watchlist functionality for redirecting on different pages

--- routes/watchlist_bp.py
+++ routes/watchlist_bp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session, redirect, url_for, flash
+from flask import Blueprint, session, redirect, url_for, flash, request
 from models import db, Watchlist
 
 watchlist_bp = Blueprint('watchlist', __name__)
@@ -21,12 +21,17 @@ def add_watchlist(note_id):
 @watchlist_bp.route('/remove_watchlist/<int:note_id>', methods=['POST'])
 def remove_watchlist(note_id):
     user_id = session.get('user_id')
+    if request.method=="POST":
+        source = request.form['source']
     watchlist_item = Watchlist.query.filter_by(user_id=user_id, note_id=note_id).first_or_404()
     
     db.session.delete(watchlist_item)
     db.session.commit()
     flash('Watchlist successfully deleted!', 'success')
-    return redirect(url_for('watchlist'))
+    if source == 'explore':
+        return redirect(url_for('explore'))
+    else:
+        return redirect(url_for('watchlist'))
     
 
 
